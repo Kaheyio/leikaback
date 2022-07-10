f6b411d96fc263226bba16f2906a0e0d7a79f949
LDEV0422 <104555936+LDEV0422@users.noreply.github.com>
2022-07-10 16:24:29 +0200

    Edit welcome page

--- index.js
+++ index.js
@@ -54,11 +54,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 
-app.listen(process.env.PORT || process.env.API_PORT, () => console.log(`Server works: http://localhost:${process.env.API_PORT}/api`));
+app.listen(process.env.PORT || process.env.API_PORT, () => console.log(`Server works: http://localhost:${process.env.API_PORT}`));
 
 // display welcome to leikaback
 app.get('/', async (req, res) => {
     res.send(
-        '<div style="font-family: sans-serif"><h1 style="color: #262626; text-align: center;">Welcome to Leikaback</h1></div>'
+        '<!DOCTYPE html><html><head><title>Leika Bank API</title><link rel="icon" type="image/x-icon" href="https://cdn1.iconfinder.com/data/icons/business-finance-communication-and-healthcare/64/message-communication-phone-payment-euro-mobile-1024.png" style="width: 50px; height: 50px;"></head><body style="display: flex;align-items: center;"><div style="font-family: sans-serif; text-align: center; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); display: flex; align-items: center"><h1 style="color: #3d3d3d;">Welcome to the Leika Bank API</h1><img src="https://to-do-cdn.microsoft.com/static-assets/f2f56b7d4c72910540effed9ccddae703d8d09b94075dddfeeab6cd79def0c60/icons/welcome-right.png" alt="leika logo" style="height: 300px; filter: hue-rotate(60deg) invert(90%) grayscale();"></div></body></html>'
     )
 });
+
+// TEST = icon card + check symbol = https://cdn2.iconfinder.com/data/icons/free-simple-line-mix/48/16-Credit_Card-1024.png
