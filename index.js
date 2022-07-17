1e7852dd60d86478eb6bb696299d3874a201bb57
LDEV0422 <104555936+LDEV0422@users.noreply.github.com>
2022-07-17 13:20:32 +0200

    test cookie

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(function (req, res, next) {
     // res.setHeader('Access-Control-Allow-Origin', 'https://leika.netlify.app');
     
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
